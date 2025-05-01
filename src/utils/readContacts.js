73d3027854e5dc2d91281af09ac74a6981a72d79
Alex Vasylenko <vasilenkobox@gmail.com>
2025-05-01 19:01:08 +0300

    add 1-2 tasks

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,15 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  await fs
+    .readFile(PATH_DB, { encoding: 'utf8' })
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      console.log('Contacts:', contacts);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+};
+readContacts();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+// import { readContacts } from './readContacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, undefined, 2));
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+writeContacts();
